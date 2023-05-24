75681a26a3869a97d9b3c0bfd1671a4fe8ec2a64
nickash2 <nickash015@gmail.com>
2023-05-24 17:53:32 +0200

    Added CSP Graph for Q4

---
+++ CSP_Graph.py
@@ -0,0 +1,29 @@
+from  CSP_solver import *
+
+#The set of variables of the CSP with domains
+variables = [
+	Variable("A", domain= [1,2,3,4]),
+	Variable("B", domain= [1,2,3,4]),
+	Variable("C", domain= [1,2,3,4]),
+    Variable("D", domain= [1,2,3,4]),
+    Variable("E", domain= [1,2,3,4])
+    
+]
+
+#Here are the constraints:
+constraints = [
+	Constraint("B >= A"),
+	Constraint("A > D"),
+	Constraint("C != A"),
+    Constraint("B != C"),
+	Constraint("C != D + 1"),
+	Constraint("C != D"),
+    Constraint("D > E"),
+    Constraint("C > E")
+]
+
+#construct a csp with the variables and constraints
+csp = CSP(variables, constraints, init_node = True, init_arc= False, keep_node= True, keep_arc= False)
+
+#Solve the csp and use verbose = True in order to print the search tree
+csp.solve(verbose=True)
